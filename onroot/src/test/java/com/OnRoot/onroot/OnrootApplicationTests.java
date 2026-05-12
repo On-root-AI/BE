f0a141246fbdb6495c6a73a3411963f843454075
hs-2171395-seungyoonkim <rlatmddbs01@naver.com>
2026-05-12 17:13:08 +0900

    feat: Spring Boot 프로젝트 초기 세팅

---
+++ onroot/src/test/java/com/OnRoot/onroot/OnrootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OnRoot.onroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnrootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
